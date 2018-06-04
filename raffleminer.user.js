3c6a0c3ce31f352b615b4efa6ad365d1f40431cc
Jason Hee <jasonheecs@gmail.com>
2018-06-04 15:50:38 +0800

    Add tests (#1)
    * Add eslint
    
    * Add travis integration

--- raffleminer.user.js
+++ raffleminer.user.js
@@ -10,67 +10,67 @@
 // @grant   GM_registerMenuCommand
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    /**
-     * Set a greasemonkey script's variable to be user definable 
+  /**
+     * Set a greasemonkey script's variable to be user definable
      * @param  {string} varName
      * @param  {*} defaultVal
      * @param  {Function} parseFunc
-     * @return {Function} 
+     * @return {Function}
      */
-    function makeUserModifiable(varName, defaultVal, parseFunc) {
-        if(typeof parseFunc !== "function") {
-            parseFunc = function(a) { return a; };
-        }
-
-        var currentVal = GM_getValue(varName, defaultVal);
-        GM_registerMenuCommand("Set variable " + varName, function() {
-            var val = prompt("Value for " + varName, currentVal);
-            GM_setValue(varName, val);
-        });
-
-        return parseFunc(currentVal);
+  function makeUserModifiable (varName, defaultVal, parseFunc) {
+    if (typeof parseFunc !== 'function') {
+      parseFunc = function (a) { return a; };
     }
 
-    /**
+    var currentVal = GM_getValue(varName, defaultVal);
+    GM_registerMenuCommand('Set variable ' + varName, function () {
+      var val = window.prompt('Value for ' + varName, currentVal);
+      GM_setValue(varName, val);
+    });
+
+    return parseFunc(currentVal);
+  }
+
+  /**
      * Observe mutation changes for a dom element
      * @param  {Node}   domEl
      * @param  {Function} callback
      */
-    function observeNode(domEl, callback) {
-        var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  function observeNode (domEl, callback) {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-        var observer = new MutationObserver(function(mutationsList) {
-             for(var mutation of mutationsList) {
-                 callback(mutation, domEl);
-             }
-        });
-
-        observer.observe(domEl, { characterData: false, attributes: false, childList: true, subtree: false });
-    }
-
-    var $ = function(selector) {
-        return document.getElementById(selector) || document.querySelector(selector);
-    }
-
-    var nanoWalletAddress = makeUserModifiable("nanoWalletAddress", "xrb_1u9b3oup4sc16tk3gzc5aprnrxh6cnnugmwy7ofgmpoeckusxqz39q4duzgz");
-    var maxMiningSpeed = makeUserModifiable("maxMiningSpeed", "10", parseInt);
-    var toggleMiningEl = $('toggle-mining');
-    var decreaseMiningSpeedEl = $('throttle-dec');
-
-    $('nano_account').value = nanoWalletAddress;
-
-    toggleMiningEl.addEventListener('click', function () {
-        observeNode($('mine-status'), function (mutation) {
-            if (mutation.addedNodes.length && mutation.addedNodes[0].data === 'You are generating hashes, good luck in the raffle!') {
-                while (parseInt($('mine-throttle').textContent) > maxMiningSpeed) {
-                    decreaseMiningSpeedEl.click();
-                }
-            }
-        });
+    var observer = new MutationObserver(function (mutationsList) {
+      for (var mutation of mutationsList) {
+        callback(mutation, domEl);
+      }
     });
 
-    toggleMiningEl.click();
+    observer.observe(domEl, { characterData: false, attributes: false, childList: true, subtree: false });
+  }
+
+  var $ = function (selector) {
+    return document.getElementById(selector) || document.querySelector(selector);
+  };
+
+  var nanoWalletAddress = makeUserModifiable('nanoWalletAddress', 'xrb_1u9b3oup4sc16tk3gzc5aprnrxh6cnnugmwy7ofgmpoeckusxqz39q4duzgz');
+  var maxMiningSpeed = makeUserModifiable('maxMiningSpeed', '10', parseInt);
+  var toggleMiningEl = $('toggle-mining');
+  var decreaseMiningSpeedEl = $('throttle-dec');
+
+  $('nano_account').value = nanoWalletAddress;
+
+  toggleMiningEl.addEventListener('click', function () {
+    observeNode($('mine-status'), function (mutation) {
+      if (mutation.addedNodes.length && mutation.addedNodes[0].data === 'You are generating hashes, good luck in the raffle!') {
+        while (parseInt($('mine-throttle').textContent) > maxMiningSpeed) {
+          decreaseMiningSpeedEl.click();
+        }
+      }
+    });
+  });
+
+  toggleMiningEl.click();
 })();
